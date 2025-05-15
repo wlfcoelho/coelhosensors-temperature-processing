f4230843268dd3bf00f6ddee13c066e6a284ab98
Wilson Coelho <wlfcoelho@gmail.com>
2025-05-14 22:54:52 -0300

    new project files

---
+++ src/main/java/com/coelhoworks/coelhosensors/temperature/processing/TemperatureProcessingApplication.java
@@ -0,0 +1,13 @@
+package com.coelhoworks.coelhosensors.temperature.processing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TemperatureProcessingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TemperatureProcessingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/coelhoworks/coelhosensors/temperature/processing/TemperatureProcessingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coelhoworks.coelhosensors.temperature.processing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TemperatureProcessingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
