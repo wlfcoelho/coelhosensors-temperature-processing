274413a41470d684a03157d4f652320156b8dd92
Wilson Coelho <wlfcoelho@gmail.com>
2025-05-20 23:21:16 -0300

    criacao do controller data e yml

---
+++ src/main/java/com/coelhoworks/coelhosensors/temperature/processing/api/config/web/StringToTSIDWebConverter.java
@@ -0,0 +1,12 @@
+package com.coelhoworks.coelhosensors.temperature.processing.api.config.web;
+
+import io.hypersistence.tsid.TSID;
+import org.springframework.core.convert.converter.Converter;
+
+public class StringToTSIDWebConverter implements Converter<String, TSID> {
+
+    @Override
+    public TSID convert(String source) {
+        return TSID.from(source);
+    }
+}
---
+++ src/main/java/com/coelhoworks/coelhosensors/temperature/processing/common/IdGenerator.java
@@ -0,0 +1,19 @@
+package com.coelhoworks.coelhosensors.temperature.processing.common;
+
+
+import com.fasterxml.uuid.Generators;
+import com.fasterxml.uuid.impl.TimeBasedEpochRandomGenerator;
+
+import java.util.UUID;
+
+public class IdGenerator {
+  private static final TimeBasedEpochRandomGenerator timeBasedEpochRandomGenerator =
+          Generators.timeBasedEpochRandomGenerator();
+
+  private IdGenerator() {
+  }
+
+  public static UUID generateTimeBasedUUID() {
+    return timeBasedEpochRandomGenerator.generate();
+  }
+}
